f40e25442512d6bbc7e976023b618ea2097bfb10
Jacob Cunningham <jcun4128@gmail.com>
2024-05-28 18:41:32 -0500

    t4

---
+++ typescript-2024/net-ninja/tutorial-4/sandbox.ts
@@ -0,0 +1,14 @@
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+let numbers = [10, 20, 30, 40];
+let mixed = ['yo', 1, true];
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+// can't add more properties
+// can override but need same properties
