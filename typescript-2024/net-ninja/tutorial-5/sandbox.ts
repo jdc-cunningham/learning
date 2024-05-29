a858edc33526b4178120cb4ae8c6b404a7dc11d3
Jacob Cunningham <jcun4128@gmail.com>
2024-05-29 14:17:18 -0500

    t5

---
+++ typescript-2024/net-ninja/tutorial-5/sandbox.ts
@@ -0,0 +1,21 @@
+// explicit types
+let character: string;
+
+// arrays
+let ninjas: string[];
+
+// need to initialize empty array to push eg. ...string[] = [];
+
+// union type (1 of 2 or 1 of 3 types)
+// pipe, or
+let mixed: (string | number)[] = [];
+let uid: string | number;
+
+// objects
+let ninjaOne: object;
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColor: string
+};
