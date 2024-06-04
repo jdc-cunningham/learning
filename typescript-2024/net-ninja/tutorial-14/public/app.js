c0773e3f6375c3adc5e0da00b33c8d708cc0d72d
Jacob Cunningham <jcun4128@gmail.com>
2024-06-04 14:44:12 -0500

    t14

---
+++ typescript-2024/net-ninja/tutorial-14/public/app.js
@@ -0,0 +1,17 @@
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+// form is Element
+const form = document.querySelector('.new-item-form'); // HTMLFormElement vs. Element, class not necessarily a form
+// type cast
+// form2 is HTMLFormElement
+const form2 = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
